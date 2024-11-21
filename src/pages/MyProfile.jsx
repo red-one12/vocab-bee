593e2fb757536c47caf333e1214d9c4cd8e0a430
red-one12 <radoanahmed1242000@gmail.com>
2024-11-21 13:19:53 +0600

    profile loading problem solve

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,14 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Link } from "react-router-dom";
 
 const MyProfile = () => {
   const { user } = useContext(AuthContext);
-  console.log(user);
-  
+  const [loading, setLoading] = useState(true);
 
+
+
+
+
+  useEffect(() => {
+    if (user) {
+      setLoading(false);
+    }
+  }, [user]);
+
+  if (loading) {
+    return;
+  }
 
   return (
+
+
+
+
     <div className="max-w-7xl m-auto flex flex-col justify-center pt-8">
       <h1 className="text-5xl md:text-7xl text-center">Welcome {user.displayName}!</h1>
       <div className="flex flex-col items-center mt-8">
@@ -17,6 +33,8 @@ const MyProfile = () => {
         <p className="mt-2 text-xl font-bold">Name: {user.displayName}</p>
         <Link to='/profile' className="mt-4 btn bg-blue-500 text-white">Update Profile</Link>
       </div>
+
+
 
     </div>
   );
